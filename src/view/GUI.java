7bccbc5a8924695e8f44229ce311c1bd24a15907
hannameden <hannameden@gmail.com>
2020-02-20 15:14:13 +0100

    nm tbh

--- src/view/GUI.java
+++ src/view/GUI.java
@@ -1,19 +1,20 @@
 package view;
 
+import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Image;
 import java.awt.Insets;
 import java.awt.image.BufferedImage;
-import java.awt.image.ImageObserver;
-import java.awt.image.ImageProducer;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
@@ -21,7 +22,10 @@ import javax.swing.UnsupportedLookAndFeelException;
 public class GUI {
 	
 	JFrame frame;
+	JPanel panel;
 	GridBagConstraints gbc;
+	Graphics g;
+	Canvas canvas;
 	private BufferedImage image;
 
 	public GUI() {
@@ -43,7 +47,9 @@ public class GUI {
 		gbc.gridx = 0;
 		gbc.gridy = 0;
 		
+		
 		frame = new JFrame();
+		canvas = new Canvas();
 
 		frame.setTitle("SpaceShots");
 		frame.setSize(800, 600);
@@ -52,11 +58,46 @@ public class GUI {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(new GridBagLayout());
 
+		panel = new JPanel();
+		
 		try {
 			image = ImageIO.read(new File("assets\\rocket.png"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
+		JLabel picLabel = new JLabel(new ImageIcon(image));
+		//frame.add(picLabel);
+		
+		Image dimg = image.getScaledInstance(30, 30,
+		        Image.SCALE_SMOOTH);
+		
+		canvas.setBackground(Color.black);
+		
+		ImageIcon imageIcon = new ImageIcon(dimg);
+		picLabel = new JLabel(new ImageIcon(dimg));
+		
+		
+		panel.add(new JLabel("hello"));
+		
+		panel.add(canvas);
+		//canvas.setBackground(Color.black);
+		frame.add(canvas);
+		
+		panel.setBackground(Color.black);
+
+		
+		canvas.setSize(800, 600);
+		
+		
+		panel.add(canvas);
+		panel.add(picLabel);
+		frame.add(panel);
+		
+		frame.add(picLabel);
+		
+		 
+		
 		
 		frame.setVisible(true);
 		
