1782f74301c91c2862c86287d5cbe855f9474810
NiklasNordgren <39832486+NiklasNordgren@users.noreply.github.com>
2020-03-05 10:18:12 +0100

    Added Asteroid

---
+++ src/model/Asteroid.java
@@ -0,0 +1,25 @@
+package model;
+
+import java.awt.Graphics;
+
+public class Asteroid extends Entity {
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void render(Graphics g) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void checkEdgeCollision(Entity e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
